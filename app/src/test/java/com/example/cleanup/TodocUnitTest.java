6bbc9d78a55a80cfec33cb513c599dca23107707
Glenn-Richard <bglennrichard.pro@yahoo.com>
2021-04-27 23:09:39 +0200

    Unit test

--- app/src/test/java/com/example/cleanup/TodocUnitTest.java
+++ app/src/test/java/com/example/cleanup/TodocUnitTest.java
@@ -1,5 +1,17 @@
 package com.example.cleanup;
 
-public class TodocUnitTest {
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class TodocUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 }
